148d2fa11b4c9be3fa86f7278c5b4335a32e5e15
Harshal-HD <harshaldukare760@gmail.com>
2026-03-28 14:28:44 +0530

    Create ec2.tf

---
+++ module/ec2/ec2.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "vm" {
+    ami = var.ami_id
+    instance_type = var.ins_type
+    key_name = var.key
+    subnet_id = var.subnet_id
+    vpc_security_group_ids = [var.security_group_id]
+        user_data = <<-EOF
+     #!/bin/bash 
+     sudo -i
+     yum install httpd -y 
+     systemctl start httpd 
+     systemctl enable httpd 
+     echo "Terraform Modules" > /var/www/html/index.html
+    EOF
+
+    tags = {
+        Name = "TF-Server"
+    }
+  
+}
